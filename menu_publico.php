f3b939136ebad1100972f44f2fa00b25ffecfaaf
BiancaLouS <biancalourencosilva@gmail.com>
2023-01-19 16:45:35 -0300

    criação do arquivo produtos

--- menu_publico.php
+++ menu_publico.php
@@ -37,7 +37,63 @@ $rows_tipos = $lista_tipos->fetch_all();
             <!-- fecha agrupamento mobile  -->
             
             <!-- nav direita  -->
-            
+
+            <div class="collapse navbar-collapse" id="menupublico">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active">
+                        <a href="index.php">
+                            <span class="glyphicon glyphicon-home"></span>
+                        </a>
+                    </li>
+                    <li> <a href="index.php#destaques">Destaques</a> </li>
+
+                    <li> <a href="index.php#produtos">Produtos</a> </li>
+
+                    <!-- dropdown  -->
+
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            
+                            Tipos
+                            <span class="caret"></span>
+
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <?php foreach($rows_tipos as $row){?>
+                                <li> <a href="produtos_por_tipo.php?id_tipo=<?php echo $row[0] ?> " > <?php echo $row[2]?></a> </li>
+                            <?php }?>
+                        </ul>
+
+                    </li>
+                        
+                        <!-- Frim Drop Down  -->
+
+                    <li> <a href="index.php#contato">Contato</a></li>
+
+                        <!-- Início Formulário de Busca  -->
+
+                        <form action="produtos_busca.php" method="get" name="form-busca" id="form-busca" class="navbar-form navbar-left" role="search">
+
+                                <div class="input-group">
+                                    <input type="search" name="buscar" id="buscar" size="9" class="form-control"
+                                    aria-label="search" placeholder="Buscar Produto" required>
+
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-default" type="submit">
+                                            <span class="glyphicon glyphicon-search"></span>
+                                        </button>
+                                    </div>
+
+                                </div>
+
+                        </form>
+                        
+                        <!-- Fim do Formulário de Busca  -->
+
+                    <li></li>
+                </ul>
+            </div>
 
         </div>
     </nav>
