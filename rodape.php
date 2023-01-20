6cf7a3b4bd859773a70924d28d4b2c2a540b37ea
BiancaLouS <biancalourencosilva@gmail.com>
2023-01-20 14:34:28 -0300

    Produzindo rodapé

--- rodape.php
+++ rodape.php
@@ -1,1 +1,69 @@
 <?php ?>
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>rodapé</title>
+</head>
+<body class="fundofixo">
+    <div class="row panel-footer fundo-rodape">
+        
+        <!-- Area de Localização  -->
+
+        <div class="col-sm-6 col-md-4"> <!--  6=Altura 4=Coluna -->
+            
+            <div class="panel-footer" style="background: none;">
+                
+                <img  width="80px" src="images/logo.png" alt="logo-pequeno">
+                <br>
+                <i> <strong>O Melhor Churrasco da Zona Leste</strong> </i> 
+                
+                <address>
+                            
+                        <i>Avenida Itaquera, 8266 - Itaquera - São Paulo - SP - CEP: 0829-500</i> 
+
+                        <br>
+                        
+                        <span class="glyphicon glyphicon-phone-alt"></span>
+                        &nbsp;(11) 2185-9200
+
+                        <br>
+
+                        <span class="glyphicon glyphicon-envelope"></span>
+                        &nbsp;
+                        <a href="mailto:contato@contatochuletaquente.com.br?subject=Contato da Churrascaria&cc=biancalourencosilva@gmail.com">
+                            Contato@chuletaquente.com.br
+                        </a>
+
+                </address>
+
+                <div class="embed-responsive embed-responsive-4by3">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.7942870068123!2d-46.45873724919619!3d-23.539900184618393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce66bf22458913%3A0xecdac462b58a9475!2sSenac%20Itaquera!5e0!3m2!1spt-BR!2sbr!4v1674235618882!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                
+                </div>
+
+            </div>
+
+        </div>
+        
+        <!-- Final da Area de Localização  -->
+
+        <div class="col-sm-6 col-md-4">
+            <div class="panel-footer">
+                <h4>Links</h4>
+                <ul class="nav nav-pills nav-stacked">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+    
+</body>
+</html>
