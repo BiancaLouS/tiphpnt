5653bad35685530d32671533e3c707e87e152ccc
BiancaLouS <biancalourencosilva@gmail.com>
2023-01-20 17:25:51 -0300

    Manutenção efetuada

--- produtos_por_tipo.php
+++ produtos_por_tipo.php
@@ -1,9 +1,11 @@
 <?php 
+
     include "conn/connect.php";
     $idTipo = $_GET['id_tipo'];
     $listaPorTipo = $conn->query("select * from vw_tbprodutos where id_tipo_produto = $idTipo;");
     $rowPorTipo = $listaPorTipo->fetch_assoc();
-    $numRows = $listaPorTipo->$num_rows;
+    $numRows = $listaPorTipo->num_rows;
+
 ?>
 
 <!DOCTYPE html>
