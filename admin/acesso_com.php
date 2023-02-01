40a13980c9676454a0ad80b24a6f448ba17534b5
BiancaLouS <biancalourencosilva@gmail.com>
2023-02-01 15:00:17 -0300

    Finalizando config do acesso_com.php

--- admin/acesso_com.php
+++ admin/acesso_com.php
@@ -11,5 +11,18 @@
 
     // Verificar se o usuário esta logado na sessão 
 
-    if(!isset($_SESSION['login_user'])) // Se não existir sessão definida para esse cara, redirecionamos a sessão por Segurança
+    if(!isset($_SESSION['login_user'])){ // Se não existir sessão definida para esse cara, redirecionamos a sessão por Segurança
+        header('location: login.php');
+        exit;
+    }
+
+    $nome_da_sessao = session_name();
+        if(!isset($_SESSION['nome_da_sessao'])
+            OR ($_SESSION['nome_da_sessao']!=$nome_da_sessao)
+        ){
+            session_destroy();
+            header('location: login.php');
+            exit;
+        }
+
 ?>
