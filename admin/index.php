ba9a8e1dba9f6adb2da5d65a867764412e365c9f
BiancaLouS <biancalourencosilva@gmail.com>
2023-02-01 14:15:03 -0300

    Adicionando novos dois arquivos a pasta admin

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,18 @@
 <?php
     include "acesso_com.php"
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Área Administrativa</title>
+</head>
+<body>
+    <?php include 'menu_adm.php'; ?>
+    <?php include 'adm_options.php'; ?>
+
+</body>
+</html>
